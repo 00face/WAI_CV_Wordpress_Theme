36b152084b92ccc5ec213e61414ea4b13af75074
Armando <adcornaglia@gmail.com>
2020-07-12 21:04:20 -0500

    Update functions.php
    Remove Gutenberg Block Library CSS from loading on the frontend

--- functions.php
+++ functions.php
@@ -3,6 +3,16 @@ if (!is_admin()) {
 }
 ?>
 
+<?php
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+?>
+
 
 <? 
 remove_action('wp_head', 'print_emoji_detection_script', 7);
